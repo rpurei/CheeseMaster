4920faa2eec6b8ba1e3bbfd48996afb19311fbb0
Purey Roman <purey.rp@zdmail.ru>
2022-08-30 10:39:45 +0300

    Added CORS headers

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ from config import APP_HOST, APP_PORT
 from endpoints.api import router as api_router
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
 
 app = FastAPI(docs_url='/api/docs',
               openapi_url='/api/openapi.json')
 app.include_router(api_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host=APP_HOST, port=APP_PORT)
