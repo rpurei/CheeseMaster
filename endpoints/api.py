7f2d7f8ba95c6c434df2705d79b0e278527ea3c8
Purey Roman <purey.rp@zdmail.ru>
2022-09-05 13:55:52 +0300

    Fixed missed Storage routes

--- endpoints/api.py
+++ endpoints/api.py
@@ -8,6 +8,7 @@ from .product.routes import router as product_router
 from .production.routes import router as production_router
 from .warehouse.routes import router as warehouse_router
 from .users.routes import router as user_router
+from .storage.routes import router as storage_router
 from fastapi import APIRouter
 
 router = APIRouter(prefix='/api')
@@ -27,3 +28,4 @@ router.include_router(product_router)
 router.include_router(production_router)
 router.include_router(warehouse_router)
 router.include_router(user_router)
+router.include_router(storage_router)
