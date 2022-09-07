cfd8ae7e16e3a66510fc726a613c86dfc55013ff
Purey Roman <purey.rp@zdmail.ru>
2022-09-07 13:34:53 +0300

    Temp disabled Category RBAC

--- endpoints/category/routes.py
+++ endpoints/category/routes.py
@@ -15,7 +15,8 @@ router = APIRouter(
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-async def add_category(category: CategoryIn, current_user=Security(get_current_user, scopes=['admin'])):
+async def add_category(category: CategoryIn):
+    # , current_user = Security(get_current_user, scopes=['admin'])
     try:
         connection = pymysql.connect(host=DB_HOST,
                                      user=DB_USER,
@@ -46,8 +47,9 @@ async def add_category(category: CategoryIn, current_user=Security(get_current_u
 
 
 @router.patch('/{category_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def update_category(category: CategoryIn, category_id: int, current_user=Security(get_current_user,
-                                                                                        scopes=['admin'])):
+async def update_category(category: CategoryIn, category_id: int):
+    # , current_user = Security(get_current_user,
+    #                           scopes=['admin'])
     try:
         connection = pymysql.connect(host=DB_HOST,
                                      user=DB_USER,
@@ -86,7 +88,8 @@ async def update_category(category: CategoryIn, category_id: int, current_user=S
 
 
 @router.delete('/{category_id}', status_code=status.HTTP_200_OK)
-async def delete_category(category_id: int, current_user=Security(get_current_user, scopes=['superadmin'])):
+async def delete_category(category_id: int):
+    # , current_user = Security(get_current_user, scopes=['superadmin'])
     try:
         connection = pymysql.connect(host=DB_HOST,
                                      user=DB_USER,
@@ -117,7 +120,8 @@ async def delete_category(category_id: int, current_user=Security(get_current_us
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def get_categories(current_user=Security(get_current_user, scopes=['admin', 'user:read', 'cheesemaster:read'])):
+async def get_categories():
+    # current_user = Security(get_current_user, scopes=['admin', 'user:read', 'cheesemaster:read'])
     try:
         connection = pymysql.connect(host=DB_HOST,
                                      user=DB_USER,
@@ -141,9 +145,10 @@ async def get_categories(current_user=Security(get_current_user, scopes=['admin'
 
 
 @router.get('/{category_id}', status_code=status.HTTP_200_OK, response_model=CategoryOut)
-async def get_category(category_id: int, current_user=Security(get_current_user, scopes=['admin',
-                                                                                         'user:read',
-                                                                                         'cheesemaster:read'])):
+async def get_category(category_id: int):
+    # , current_user = Security(get_current_user, scopes=['admin',
+    #                                                     'user:read',
+    #                                                     'cheesemaster:read'])
     try:
         connection = pymysql.connect(host=DB_HOST,
                                      user=DB_USER,
